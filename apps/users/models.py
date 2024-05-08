66f29fba17c527d2a570c761e8c503c90a85970e
alvezgr <alvezpope@gmail.com>
2024-05-08 18:32:05 -0300

    Created an abstract user to be more flexible when needed to modify users data.

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,40 @@
-from django.db import models
+"""User model."""
 
-# Create your models here.
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Utilities
+from apps.utils.models import GPBaseModel
+
+
+class User(GPBaseModel, AbstractUser):
+    """User model.
+    Extend from Django's Abstract User, change the username field
+    to email and add some extra fields.
+    """
+
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        }
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    is_verified = models.BooleanField(
+        'verified',
+        default=True,
+        help_text='Set to true when the user have verified its email address.'
+    )
+
+    def __str__(self):
+        """Return a str representation of User."""
+        return f'User: {self.username}'
+
+    def get_short_name(self):
+        """Return username."""
+        return self.username
