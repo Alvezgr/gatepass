34b2388ea238e74006ee64db58a4393f76638c3d
alvezgr <alvezpope@gmail.com>
2024-05-10 23:31:23 -0300

    Added tests for Gates and fixed neighborhood tests.

--- apps/neighborhood/tests.py
+++ apps/neighborhood/tests.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 # Models
-from apps.neighborhood.models import Neighborhood
+from apps.neighborhood.models import Neighborhood, Gate
 from apps.users.models import User
 
 
@@ -56,7 +56,7 @@ class NeighborhoodTests(APITestCase):
 
     def test_fail_create_neighborhood_created(self):
         """
-        Ensure an already created with a license plate neighborhood can't be created again.
+        Ensure an already created neighborhood can't be created again.
         """
         url = reverse("neighborhoods:neighborhoods-list")
 
@@ -70,7 +70,7 @@ class NeighborhoodTests(APITestCase):
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
-            response.data["license_plate"][0],
+            response.data["name"][0],
             "Este vecindario ya existe.",
         )
 
@@ -89,7 +89,7 @@ class NeighborhoodTests(APITestCase):
         self.client.force_authenticate(user=self.user)
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data["license_plate"], data["license_plate"])
+        self.assertEqual(response.data["name"], data["name"])
 
     def test_update_neighborhood(self):
         """
@@ -104,4 +104,108 @@ class NeighborhoodTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
             response.data["max_vehicles"], data["max_vehicles"]
+        )
+
+
+class GatesTests(APITestCase):
+    """Test cases for gates"""
+
+    def setUp(self):
+        """Default setting for testing gates neighborhoods."""
+        self.neighborhood = Neighborhood.objects.create(
+            name="La emilia",
+            address="Road 12",
+            max_vehicles=10
+        )
+        self.gate = Gate.objects.create(
+            number=1,
+            code="AG22",
+            neighborhood=self.neighborhood
+        )
+        self.user = User.objects.create(
+            username="gera",
+            email="gera@gatepass.com.ar"
+
+        )
+
+    def test_fail_unathorized_user_create_gate(self):
+        """
+        Ensure an unauthorized user can't create a gate.
+        """
+        url = reverse("neighborhoods:gates-list")
+        data = {
+            "number": 2,
+            "code": "KAF33",
+            "neighborhood": self.neighborhood.pk,
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_fail_create_gate_without_nighborhood(self):
+        """
+        Ensure we must provide a neighborhood to create a gate.
+        """
+        url = reverse("neighborhoods:gates-list")
+
+        data = {
+            "number": 2,
+            "code": "KAF33",
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["name"][0], "This field is required.")
+
+    def test_fail_create_gate(self):
+        """
+        Ensure an already created gate for a neighborhood can't be created again.
+        """
+        url = reverse("neighborhoods:gates-list")
+
+        data = {
+            "number": 1,
+            "code": "KAF33",
+            "neighborhood": self.neighborhood.pk
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            response.data["error"][0],
+            "Ya existe una puerta creada para este vecindario.",
+        )
+
+    def test_create_gate(self):
+        """
+        Ensure we can create a gate.
+        """
+        url = reverse("neighborhoods:gates-list")
+        
+        data = {
+            "number": 4,
+            "code": "KAF33",
+            "neighborhood": self.neighborhood.pk
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["number"], data["number"])
+
+    def test_update_neighborhood(self):
+        """
+        Ensure we can update a gate.
+        """
+        url = reverse("neighborhoods:gates-detail", kwargs={"pk": self.gate.pk})
+
+        data = {"code": "LLA12"}
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.patch(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data["code"], data["code"]
         )
